c231757e2731865b2fabc519dc17455a62566011
Volodymyr_Tryhub <tryhub9@gmail.com>
2026-02-26 22:22:21 +0200

    finish maybe

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,44 @@
-console.log('Hello, Form page!');
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+  formData = JSON.parse(savedData);
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+
+  if (name === 'email' || name === 'message') {
+    formData[name] = value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (formData.email.trim() === '' || formData.message.trim() === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  formData = { email: '', message: '' };
+  form.reset();
+});
+console.log(
+  'Form data will be logged to the console upon submission. Please fill in the form and submit to see the output.'
+);
